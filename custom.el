c4c5f8d10270bf3ece539767c9a9556691e9e5b2
Ryan Jung <ryan.matthew.jung@gmail.com>
2022-01-12 09:47:40 -0500

    init

---
+++ custom.el
@@ -0,0 +1,83 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(awesome-tray-mode-line-active-color "#2fafff")
+ '(awesome-tray-mode-line-inactive-color "#323232")
+ '(custom-safe-themes
+   '("d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(exwm-floating-border-color "#262626")
+ '(fci-rule-color "#4F4F4F")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-themes-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-themes-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-themes-fringe-yellow))
+ '(highlight-tail-colors ((("#454845") . 0) (("#474f4f") . 20)))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#c0c530")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae2f0")
+     ("DONT" . "#70b900")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#d3b55f")
+     ("KLUDGE" . "#d0bc00")
+     ("HACK" . "#d0bc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9077")
+     ("XXX+" . "#ef8b50")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-themes-pseudo-header)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#8CD0D3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
+ '(objed-cursor-color "#CC9393")
+ '(org-src-block-faces 'nil)
+ '(pdf-view-midnight-colors (cons "#DCDCDC" "#3F3F3F"))
+ '(rustic-ansi-faces
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(vc-annotate-background "#3F3F3F")
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7F9F7F")
+    (cons 40 "#a4b48f")
+    (cons 60 "#cac99f")
+    (cons 80 "#F0DFAF")
+    (cons 100 "#eacfa4")
+    (cons 120 "#e4bf99")
+    (cons 140 "#DFAF8F")
+    (cons 160 "#dea3a0")
+    (cons 180 "#dd97b1")
+    (cons 200 "#DC8CC3")
+    (cons 220 "#d68eb3")
+    (cons 240 "#d190a3")
+    (cons 260 "#CC9393")
+    (cons 280 "#ab8080")
+    (cons 300 "#8a6d6d")
+    (cons 320 "#695b5b")
+    (cons 340 "#4F4F4F")
+    (cons 360 "#4F4F4F")))
+ '(vc-annotate-very-old-color nil)
+ '(xterm-color-names
+   ["black" "#ff8059" "#44bc44" "#d0bc00" "#2fafff" "#feacd0" "#00d3d0" "gray65"])
+ '(xterm-color-names-bright
+   ["gray35" "#ef8b50" "#70b900" "#c0c530" "#79a8ff" "#f78fe7" "#4ae2f0" "white"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
